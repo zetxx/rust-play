6ac62e73bfa970ae4acb21128ac1f179e3f14890
Elin Angelow <elin.angelov@shelly.com>
2025-03-25 11:05:32 +0200

    ..

--- rust-unofficial.github.io/target/debug/deps/lists-2fd06f73b4771e77.d
+++
@@ -1,6 +0,0 @@
-/store/Projects/rust/lists/target/debug/deps/lists-2fd06f73b4771e77: src/lib.rs src/first.rs
-
-/store/Projects/rust/lists/target/debug/deps/lists-2fd06f73b4771e77.d: src/lib.rs src/first.rs
-
-src/lib.rs:
-src/first.rs:
--- rust-unofficial.github.io/target/debug/deps/lists-50b31a88d4179fc6.d
+++
@@ -1,5 +0,0 @@
-/store/Projects/rust/lists/target/debug/deps/liblists-50b31a88d4179fc6.rmeta: src/lib.rs
-
-/store/Projects/rust/lists/target/debug/deps/lists-50b31a88d4179fc6.d: src/lib.rs
-
-src/lib.rs:
